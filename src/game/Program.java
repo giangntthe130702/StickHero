e57f71daa6516dd64e1c446495a3f3d17a551aab
giang <giangntthe130702@fpt.edu.vn>
2019-06-18 17:07:56 +0700

    Done

---
+++ src/game/Program.java
@@ -0,0 +1,60 @@
+package game;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by huynq on 7/4/17.
+ */
+public class Program {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        GamePanel panel = new GamePanel();
+        panel.setBackground(Color.CYAN);
+        panel.setPreferredSize(new Dimension(Settings.GAME_WIDTH, Settings.GAME_HEIGHT));
+
+        window.add(panel); // location
+        window.pack();
+        window.setTitle("Game Stick Hero");
+//        window.setSize(800, 600);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        KeyAdapter keyHandler = new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                KeyEventPress.isAnyKeyPress = true;
+
+                if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+                    KeyEventPress.isFirePress = true;
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                KeyEventPress.isAnyKeyPress = false;
+
+                if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+                    KeyEventPress.isFirePress = false;
+                }
+            }
+        };
+        window.addKeyListener(keyHandler);
+        // mouse
+
+        window.setVisible(true);
+
+        panel.gameLoop();
+
+        // ctrl + ? : comment/uncomment
+        // alt + enter : sua loi code
+        // (fn +) shift + f6 : doi ten
+        // alt + ctrl + l : format code
+    }
+}
---
+++ src/game/renderer/Renderer.java
@@ -0,0 +1,89 @@
+package game.renderer;
+
+import game.GameObject;
+import tklibs.SpriteUtils;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File; // file | directory
+import java.util.ArrayList;
+
+public class Renderer {
+    public BufferedImage image;
+
+    public ArrayList<BufferedImage> images;
+    public int currentImageIndex;
+    public int frameCount;
+    public boolean isOnce;
+
+    public Renderer(String url) {
+        File source = new File(url);
+        if(source.exists() && source.isFile()) {
+            image = SpriteUtils.loadImage(url);
+        }
+        if(source.exists() && source.isDirectory()) {
+            images = SpriteUtils.loadImages(url);
+            currentImageIndex = 0;
+            frameCount = 0;
+        }
+    }
+
+    public Renderer() {
+
+    }
+
+    public Renderer(String url, boolean isOnce){
+        this(url);
+        this.isOnce = isOnce;
+    }
+
+    public void render(Graphics g, GameObject master) {
+        if(image != null) {
+            // draw single image
+            drawImage(g, image, master);
+        }
+        if(images != null) {
+            // draw animation
+            BufferedImage currentImage = images.get(currentImageIndex);
+            drawImage(g, currentImage, master);
+            frameCount++;
+            if(frameCount > 6) {
+                currentImageIndex++;
+                if(currentImageIndex >= images.size()) {
+                    if(isOnce){
+                        master.deactive();
+                    }
+                    currentImageIndex = 0;
+                }
+                frameCount = 0;
+            }
+        }
+    }
+
+    private void drawImage(Graphics g, BufferedImage image
+        , GameObject master) {
+        // image.getWidth()
+        // master.anchor.x
+        // image.getHeight()
+        // master.anchor.y
+        g.drawImage(
+                image,
+//                (int) master.position.x,
+//                (int) master.position.y,
+                (int) (master.position.x - master.anchor.x * image.getWidth()),
+                (int) (master.position.y - master.anchor.y * image.getHeight()),
+                null
+        );
+        // draw hitBox for master
+        // useful when developing
+//        if (master.hitBox != null) {
+//            g.setColor(Color.GREEN);
+//            g.drawRect(
+//                    (int) master.hitBox.left(),
+//                    (int) master.hitBox.top(),
+//                    master.hitBox.width,
+//                    master.hitBox.height
+//            );
+//        }
+    }
+}
